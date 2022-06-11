c7bc23cff3de20fc4e9298c430afb2d40a864069
RAJ KUMAR DUTTA <rajkumarsindri22@gmail.com>
2022-06-11 21:11:19 +0530

    Day-1 Data Structure and Algorithm(Logic Building [Progress-To-Do]

--- Day-1/Find_theDuplicate_Number.cpp
+++ Day-1/Find_theDuplicate_Number.cpp
@@ -4,18 +4,12 @@ using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-        do{
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        }while(slow!=fast);
-        fast = nums[0];
-        while(slow != fast){
-            slow = nums[slow];
-            fast = nums[fast];
+        for(int i = 0; i<nums.size();i++){
+            int index = abs(nums[i]);
+            if(nums[index] < 0) return index;
+             nums[index] *= -1;
         }
-        return slow;
+        return -1;
     }
 };
 
