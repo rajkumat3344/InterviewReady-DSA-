dfd3714eed0a87d06169b8c15388fbad27297a08
RAJ KUMAR DUTTA <rajkumarsindri22@gmail.com>
2022-06-13 00:30:39 +0530

    Day-1 Data Structure and Algorithm(Logic Building [Progress-To-Do]

--- Day-1/Merge_without_ES.cpp
+++ Day-1/Merge_without_ES.cpp
@@ -8,13 +8,16 @@ class Solution{
 public:
 	void merge(int arr1[], int arr2[], int n, int m) {
 	    // code here
-        for(int i=0; i<n; i++){
-            if(arr1[i] > arr2[0]){
-                swap(arr1[i], arr2[0]);
+	    int i=n-1, j=0;
+        while(i>=0 && j<m){
+            if(arr1[i] > arr2[j]){
+                swap(arr1[i], arr2[j]);
             }
-            sort(arr1,arr1 + n);
-            sort(arr2,arr2 + m);
+            i--;
+            j++;
         }
+        sort(arr1,arr1 + n);
+        sort(arr2,arr2 + m);
 	}
 };
 
