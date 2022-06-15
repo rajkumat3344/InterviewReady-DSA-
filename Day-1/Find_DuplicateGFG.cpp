9863d9f05d55a6d32a44914697e57be30b972b2b
RAJ KUMAR DUTTA <rajkumarsindri22@gmail.com>
2022-06-16 00:26:47 +0530

    Day-1 Data Structure and Algorithm(Logic Building [Progress-To-Do]

---
+++ Day-1/Find_DuplicateGFG.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+/*Algorithm: 
+Traverse the given array from start to end.
+For every element in the array increment the arr[i]%n‘th element by n.
+Now traverse the array again and print all those indexes i for which arr[i]/n is greater than 1. Which guarantees that the number n has been added to that index
+This approach works because all elements are in the range from 0 to n-1 and arr[i] would be greater than n only if a value “i” has appeared more than once.*/
+ 
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int remainder = arr[i]%n;
+            arr[remainder] += n;
+        }
+        for(int i=0;i<n;i++){
+            int quotient = arr[i]/n;
+            if(quotient >= 2) ans.push_back(i);
+        }
+        if(ans.size() == 0){
+            ans.push_back(-1);
+            return ans;
+        }
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
